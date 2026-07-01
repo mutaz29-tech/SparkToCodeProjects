0125c0fdd4587a3b1c3a62e2ec21cf3b5ac76b07
mutaz29 <mutaz6991@gmail.com>
2026-07-01 20:02:25 +0400

    task2 solution

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Task1
+﻿using System.Reflection.Metadata;
+
+namespace Task1
 {
     internal class Program
     {
@@ -6,18 +8,29 @@
         {
             // task1
             //Personal Info Card    
-            Console.WriteLine("entre Your name ");
-            String name = Console.ReadLine();
-            Console.WriteLine("entre Your age ");
-            int age = int.Parse(Console.ReadLine());
-            Console.WriteLine("entre Your Height ");
-            double height = double.Parse(Console.ReadLine());
-            bool student = true;
-            Console.WriteLine("name " +name);
-            Console.WriteLine("age " + age);
-            Console.WriteLine("height " + height);
-            Console.WriteLine("student  " + student);
+            //Console.WriteLine("entre Your name ");
+            //String name = Console.ReadLine();
+            //Console.WriteLine("entre Your age ");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.WriteLine("entre Your Height ");
+            //double height = double.Parse(Console.ReadLine());
+            //bool student = true;
+            //Console.WriteLine("name " +name);
+            //Console.WriteLine("age " + age);
+            //Console.WriteLine("height " + height);
+            //Console.WriteLine("student  " + student);
 
+            ///task 2
+            /// Rectangle Calculator
+            /// 
+            Console.WriteLine("entre length  ");
+            double length = double.Parse(Console.ReadLine());
+            Console.WriteLine("entre width  ");
+            double width = double.Parse(Console.ReadLine());
+            double Area = length * width;
+            double Perimeter = 2 * (length + width);
+            Console.WriteLine("Area: " + Area);
+            Console.WriteLine("Perimeter: " + Perimeter);
 
 
         }
