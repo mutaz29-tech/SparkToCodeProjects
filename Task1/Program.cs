41cb5075de0a4775fe8026403822eb8e21584a10
mutaz29 <mutaz6991@gmail.com>
2026-07-01 19:56:09 +0400

    task1 soluation

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // task1
+            //Personal Info Card    
+            Console.WriteLine("entre Your name ");
+            String name = Console.ReadLine();
+            Console.WriteLine("entre Your age ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("entre Your Height ");
+            double height = double.Parse(Console.ReadLine());
+            bool student = true;
+            Console.WriteLine("name " +name);
+            Console.WriteLine("age " + age);
+            Console.WriteLine("height " + height);
+            Console.WriteLine("student  " + student);
+
+
+
+        }
+    }
+}
