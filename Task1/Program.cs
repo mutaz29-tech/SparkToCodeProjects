aae83f87d6f8c946f331adf454877826d976a324
mutaz29 <mutaz6991@gmail.com>
2026-07-01 20:07:25 +0400

    task 3 solution

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -23,15 +23,28 @@ namespace Task1
             ///task 2
             /// Rectangle Calculator
             /// 
-            Console.WriteLine("entre length  ");
-            double length = double.Parse(Console.ReadLine());
-            Console.WriteLine("entre width  ");
-            double width = double.Parse(Console.ReadLine());
-            double Area = length * width;
-            double Perimeter = 2 * (length + width);
-            Console.WriteLine("Area: " + Area);
-            Console.WriteLine("Perimeter: " + Perimeter);
+            //Console.WriteLine("entre length  ");
+            //double length = double.Parse(Console.ReadLine());
+            //Console.WriteLine("entre width  ");
+            //double width = double.Parse(Console.ReadLine());
+            //double Area = length * width;
+            //double Perimeter = 2 * (length + width);
+            //Console.WriteLine("Area: " + Area);
+            //Console.WriteLine("Perimeter: " + Perimeter);
 
+            //task 3
+            //Even or Odd Checker
+            Console.WriteLine("entre even number  ");
+            int evennum = int.Parse(Console.ReadLine());
+
+            if (evennum % 2 == 0)
+            {
+                Console.WriteLine(evennum + "number is even");
+            }
+            else
+            {
+                Console.WriteLine(evennum + "number  is odd");
+            }
 
         }
     }
