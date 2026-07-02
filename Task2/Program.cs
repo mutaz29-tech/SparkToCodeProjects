c84380003b5e2391423ca46ab85188a26cd62bba
mutaz29 <mutaz6991@gmail.com>
2026-07-02 20:12:31 +0400

    implemented problem 5 - Number Guessing Game

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -39,20 +39,46 @@
             //}
             ////////////////////////////////
             ///Task 4 - Password Retry
-            string password = "Spark2026";
-            string userPassword = "";
+            //string password = "Spark2026";
+            //string userPassword = "";
 
-            while (userPassword != password)
-            {
-                Console.Write("Enter the password :");
-                userPassword = Console.ReadLine();
-                if (userPassword != password)
-                {
-                    Console.WriteLine("Incorrect password, try again");
-                }
-            }
-            Console.WriteLine("Access Granted");
+            //while (userPassword != password)
+            //{
+            //    Console.Write("Enter the password :");
+            //    userPassword = Console.ReadLine();
+            //    if (userPassword != password)
+            //    {
+            //        Console.WriteLine("Incorrect password, try again");
+            //    }
+            //}
+            //Console.WriteLine("Access Granted");
             //////////////////
-    }
+
+            ///Task 5 - Number Guessing Game
+            int secretNumber = 29; // my secret number is 29
+            int gusss;
+            int attempts = 0;
+
+            do
+            {
+                Console.Write("Guess the secret number:");
+                gusss = int.Parse(Console.ReadLine());
+                attempts++;
+                if (gusss > secretNumber)
+                {
+                    Console.WriteLine("Too high");
+                }
+                else if (gusss < secretNumber)
+                {
+                    Console.WriteLine("Too low.");
+                }
+                else
+                {
+                    Console.WriteLine($"you gussed the secret number");
+                }
+            } while (gusss != secretNumber);
+            Console.WriteLine("It took you {attempts} attempts.");
+        }
     }
 }
+    
