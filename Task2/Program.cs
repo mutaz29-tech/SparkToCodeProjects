8ae10998862edb5c207640787c004979f33c30a9
mutaz29 <mutaz6991@gmail.com>
2026-07-02 20:01:57 +0400

    implemented problem 2 - Sum of Numbers 1 to N

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -5,15 +5,30 @@
         static void Main(string[] args)
         {
             //Task 1 - Countdown Timer
-            Console.WriteLine("Enter starting number:");
-            int number = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter starting number:");
+            //int number = int.Parse(Console.ReadLine());
 
-            for (int i = number; i >= 1; i--)
-            {
-                Console.WriteLine(i);
-            }
-            Console.WriteLine("Liftoff !");
+            //for (int i = number; i >= 1; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine("Liftoff !");
             //////////////////////////////////
+            
+
+            ///Task 2 - Sum of Numbers 1 to N
+            Console.WriteLine("Enter positive whole number:");
+            int pnum = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = 1; i <= pnum; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine($"The sum is :" + sum);
+            /////////////////////
+
         }
     }
 }
