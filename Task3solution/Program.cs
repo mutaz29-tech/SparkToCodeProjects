450719ad0b1b36d3d983e77c2d8ea08e71e14ea4
mutaz29 <mutaz6991@gmail.com>
2026-07-05 21:22:59 +0400

    implemented problem 1- Absolute Difference

---
+++ Task3solution/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task3solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1 - Absolute Difference
+            Console.WriteLine("enter first number");
+            int num1= int.Parse(Console.ReadLine());
+            Console.WriteLine("enter second number");
+            int num2 = int.Parse(Console.ReadLine());
+            double sub =Math.Abs(num1 - num2);
+            Console.WriteLine(sub);
+        }
+    }
+}
