7f06ad66a5516aebbe2d0af64db6f4962b313d26
mutaz29 <mutaz6991@gmail.com>
2026-07-05 21:30:28 +0400

    implemented problem 2 - Power & Root Explore

--- Task3solution/Program.cs
+++ Task3solution/Program.cs
@@ -5,12 +5,21 @@
         static void Main(string[] args)
         {
             //Task 1 - Absolute Difference
-            Console.WriteLine("enter first number");
-            int num1= int.Parse(Console.ReadLine());
-            Console.WriteLine("enter second number");
-            int num2 = int.Parse(Console.ReadLine());
-            double sub =Math.Abs(num1 - num2);
-            Console.WriteLine(sub);
+            //Console.WriteLine("enter first number");
+            //int num1= int.Parse(Console.ReadLine());
+            //Console.WriteLine("enter second number");
+            //int num2 = int.Parse(Console.ReadLine());
+            //double sub =Math.Abs(num1 - num2);
+            //Console.WriteLine(sub);
+            ////////////////////////////////////////
+
+            ///Task 2 - Power & Root Explore
+            Console.WriteLine("enter number");
+            int number = int.Parse(Console.ReadLine());
+            double pow = Math.Pow(number,2);
+            double sqrt = Math.Sqrt(number);
+            Console.WriteLine("power is " + pow);
+            Console.WriteLine("square root is " + sqrt);
         }
     }
 }
