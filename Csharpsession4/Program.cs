fa0c180e161fe9eeed998c6ae507178a4c779cd5
mutaz29 <mutaz6991@gmail.com>
2026-07-06 20:19:58 +0400

    add project files

---
+++ Csharpsession4/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Csharpsession4
+{
+    internal class Program
+    {
+            static void PrintWelcome(string name)
+            {
+                Console.WriteLine($"Welcome, {name}! We are glad to have you here.");
+            }
+
+            static void Main()
+            {
+                Console.Write("Enter your name: ");
+                string name = Console.ReadLine();
+
+                PrintWelcome(name);
+            }
+
+        }
+    }
+
