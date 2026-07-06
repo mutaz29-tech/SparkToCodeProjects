0b716591d4544bad6c0917647f01996b9f50125e
mutaz29 <mutaz6991@gmail.com>
2026-07-06 20:23:52 +0400

    implemented problem 2 - Square Number Function

--- Task4solution/Program.cs
+++ Task4solution/Program.cs
@@ -4,18 +4,33 @@
     {
         ///Task 1 - Personalized Welcome Function
 
-        static void PrintWelcome(string name)
-        {
-            Console.WriteLine($"Welcome, {name}! We are glad to have you here.");
-        }
+        //static void PrintWelcome(string name)
+        //{
+        //    Console.WriteLine($"Welcome, {name}! We are glad to have you here.");
+        //}
 
+        //static void Main()
+        //{
+        //    Console.Write("Enter your name: ");
+        //    string name = Console.ReadLine();
+
+        //    PrintWelcome(name);
+        //}
+        ////////////////////////////////////////////
+
+        ///Task 2 - Square Number Function
+        static int Square(int number)
+        {
+            return number * number;
+        }
         static void Main()
         {
-            Console.Write("Enter your name: ");
-            string name = Console.ReadLine();
-
-            PrintWelcome(name);
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+            int result = Square(number);
+            Console.WriteLine("The square is: " + result);
         }
+
 
     }
 }
