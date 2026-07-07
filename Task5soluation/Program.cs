40d872389c63a158c218c860d85e9eb23799c7e1
mutaz29 <mutaz6991@gmail.com>
2026-07-07 23:02:13 +0400

    implemented problem 3 - Browsing History Stack

--- Task5soluation/Program.cs
+++ Task5soluation/Program.cs
@@ -23,20 +23,40 @@
             //////////////////////////////////////////////////
 
             ///Task 2 - Dynamic To-Do List
-            List<string> todoItems = new List<string>();
-            for (int i = 0; i < 5; i++)
+            //List<string> todoItems = new List<string>();
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.Write($"Enter task #{i + 1}: ");
+            //    string task = Console.ReadLine();
+            //    todoItems.Add(task);
+            //}
+            //Console.WriteLine(" To-Do List:");
+            //int number = 1;
+            //foreach (string task in todoItems)
+            //{
+            //    Console.WriteLine($"{number}. {task}");
+            //    number++;
+            //}
+            //////////////////////////////////////////////////////////////
+
+            ///Task 3 - Browsing History Stack
+            Stack<string> browserHistory = new Stack<string>();
+            for (int i = 0; i < 3; i++)
             {
-                Console.Write($"Enter task #{i + 1}: ");
-                string task = Console.ReadLine();
-                todoItems.Add(task);
+                Console.Write($"Enter URL #{i + 1}: ");
+                string url = Console.ReadLine();
+                browserHistory.Push(url);
             }
-            Console.WriteLine(" To-Do List:");
-            int number = 1;
-            foreach (string task in todoItems)
+            browserHistory.Pop();
+            if (browserHistory.Count > 0)
             {
-                Console.WriteLine($"{number}. {task}");
-                number++;
+                Console.WriteLine($" After pressing Back, you land on: {browserHistory.Peek()}");
             }
+            else
+            {
+                Console.WriteLine(" No pages left in the browser history.");
+            }
+
 
         }
     }
