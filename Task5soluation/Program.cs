cf9d33a0f81bc2629a20a146259830e444a8e5ca
mutaz29 <mutaz6991@gmail.com>
2026-07-07 22:49:27 +0400

    add problem files

---
+++ Task5soluation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task5soluation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
