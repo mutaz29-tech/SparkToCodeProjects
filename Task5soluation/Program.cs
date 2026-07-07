b9c7cae21f9cbd30f2d8d946c21b18dbc945dce5
mutaz29 <mutaz6991@gmail.com>
2026-07-07 22:57:02 +0400

    implemented problem 2 - Dynamic To-Do List

--- Task5soluation/Program.cs
+++ Task5soluation/Program.cs
@@ -6,19 +6,36 @@
         {
             //Task 1 - Fixed Grades Array
 
-            int[] grades = new int[5];
-            // Input grades using a for loop
-            for (int i = 0; i < grades.Length; i++)
-            {
-                Console.Write($"Enter grade #{i + 1}: ");
-                grades[i] = int.Parse(Console.ReadLine());
-            }
-            // Print grades using a foreach loop
-            Console.WriteLine(" Student Grades:");
+            //int[] grades = new int[5];
+            //// Input grades using a for loop
+            //for (int i = 0; i < grades.Length; i++)
+            //{
+            //    Console.Write($"Enter grade #{i + 1}: ");
+            //    grades[i] = int.Parse(Console.ReadLine());
+            //}
+            //// Print grades using a foreach loop
+            //Console.WriteLine(" Student Grades:");
 
-            foreach (int grade in grades)
+            //foreach (int grade in grades)
+            //{
+            //    Console.WriteLine(grade);
+            //}
+            //////////////////////////////////////////////////
+
+            ///Task 2 - Dynamic To-Do List
+            List<string> todoItems = new List<string>();
+            for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine(grade);
+                Console.Write($"Enter task #{i + 1}: ");
+                string task = Console.ReadLine();
+                todoItems.Add(task);
+            }
+            Console.WriteLine(" To-Do List:");
+            int number = 1;
+            foreach (string task in todoItems)
+            {
+                Console.WriteLine($"{number}. {task}");
+                number++;
             }
 
         }
