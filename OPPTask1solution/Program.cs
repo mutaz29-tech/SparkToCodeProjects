11cf5fe997175e05365f19d3e2cb352ac8ed7194
mutaz29 <mutaz6991@gmail.com>
2026-07-13 00:05:10 +0400

    add project files

---
+++ OPPTask1solution/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OPPTask1solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
