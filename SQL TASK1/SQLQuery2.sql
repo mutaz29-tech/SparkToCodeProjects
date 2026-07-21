7011bc0ce5db86c047b4d5157dd4849870159b20
mutaz29 <mutaz6991@gmail.com>
2026-07-22 03:55:08 +0400

    CREATE TABLE Employee

--- SQL TASK1/SQLQuery2.sql
+++ SQL TASK1/SQLQuery2.sql
@@ -12,3 +12,18 @@ Mgr_ssn CHAR(9),
 Mgr_start_date DATE,
 NumberOfEmployees INT DEFAULT 0 CHECK (NumberOfEmployees >= 0)
 );
+//////////////
+////Employee
+CREATE TABLE Employee
+(
+    Ssn CHAR(9) PRIMARY KEY,
+    Fname VARCHAR(30) NOT NULL,
+    Minit CHAR(1),
+    Lname VARCHAR(30) NOT NULL,
+    Address VARCHAR(100),
+    Sex CHAR(1) NOT NULL CHECK (Sex IN ('M','F')),
+    Bdate DATE,
+    Salary DECIMAL(10,2) NOT NULL CHECK (Salary > 0),
+    Dno INT NOT NULL,
+    Super_ssn CHAR(9)
+);
