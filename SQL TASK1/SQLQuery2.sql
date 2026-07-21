e91d73a37f20524020a6fd443d6c89e30ce7977e
mutaz29 <mutaz6991@gmail.com>
2026-07-22 03:54:27 +0400

    CREATE Department Table

--- SQL TASK1/SQLQuery2.sql
+++ SQL TASK1/SQLQuery2.sql
@@ -1,5 +1,14 @@
-// Database creation script 
-CREATE DATABASE CompanyDB; 
-GO; 
-USE CompanyDB;
-//////////////////////////////////////////
+--// Database creation script 
+--CREATE DATABASE CompanyDB; 
+--GO; 
+--USE CompanyDB;
+--//////////////////////////////////////////
+///Department
+CREATE TABLE Department
+(
+Dnumber INT PRIMARY KEY,
+Dname VARCHAR(60) NOT NULL UNIQUE,
+Mgr_ssn CHAR(9),
+Mgr_start_date DATE,
+NumberOfEmployees INT DEFAULT 0 CHECK (NumberOfEmployees >= 0)
+);
